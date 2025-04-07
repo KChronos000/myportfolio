71e3cf126063ceb9a11caf1fb8ad005dbfaeba5e
KChronos000 <damnwork186@gmail.com>
2025-04-07 20:31:19 +0700

    ux/ui

--- app/components/personalinfo/GraduatedSec.tsx
+++ app/components/personalinfo/GraduatedSec.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GraduatedSec = () => {
   return (
     <section>
-        <div className="flex flex-col justify-center items-center pt-6">
+        <div className="flex flex-col justify-center items-center py-6">
             <h2 className='text-2xl font-bold my-4'>ประวัติการศึกษา</h2>
             <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 text-center">
                 <div className='hover:transition-all hover:duration-300 hover:scale-105'>

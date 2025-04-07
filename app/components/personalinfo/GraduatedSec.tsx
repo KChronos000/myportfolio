564c3040b0c68d3ec432243a9b046de2de1cbe24
KChronos000 <damnwork186@gmail.com>
2025-04-07 16:17:41 +0700

    ux/ui

--- app/components/personalinfo/GraduatedSec.tsx
+++ app/components/personalinfo/GraduatedSec.tsx
@@ -4,8 +4,8 @@ const GraduatedSec = () => {
   return (
     <section>
         <div className="flex flex-col justify-center items-center pt-6">
-            <h2 className='text-2xl font-bold'>ประวัติการศึกษา</h2>
-            <div className="grid grid-cols-2 gap-4 text-center">
+            <h2 className='text-2xl font-bold my-4'>ประวัติการศึกษา</h2>
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 text-center">
                 <div className='hover:transition-all hover:duration-300 hover:scale-105'>
                     <img src="test.jpg" width={100} className="mx-auto" />
                     <p className="mt-2">จบการศึกษาระดับชั้นอนุบาล</p>
